5e629053cdf2a8556e4738ba3ddfe24596726c73
Roberto Ojeda <ojeda.robertodaniel@gmail.com>
2022-08-18 18:13:57 -0300

    Modif. hlerpes register

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 const { login } = require("../controllers/auth.js");
 const { register } = require("../controllers/auth.js")
+const { esRoleValido} = require("../helpers/db-validators.js");
 
 const { validarCampos } = require("../middlewares/validar-campos");
 
@@ -22,6 +23,7 @@ router.post(
     check("name", "El nombre es obligatorio").not().isEmpty(),
     check("email", "El correo es obligatorio").isEmail(),
     check("password", "El password es obligatorio").not().isEmpty(),
+    check("role").custom(esRoleValido),
     validarCampos
   ],
   register
